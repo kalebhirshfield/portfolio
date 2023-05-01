9335131fa3785bb9e6ba96da8b054ce05de6309c
Kaleb Hirshfield <kaleb.hirshfield@protonmail.com>
2023-05-01 01:18:23 +0100

    Title rework

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,7 +1,7 @@
 import { seo } from "config";
 
 export default {
-  titleTemplate: `%s | ${seo.title}`,
+  titleTemplate: `${seo.title} | %s`,
   openGraph: {
     type: "website",
     locale: "en_GB",
